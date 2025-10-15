e2d9a7c4df5ef94bca12f8086f35a088750c1ed2
Vaibhav Patil <vaibhav.patil@contentstack.com>
2025-10-15 20:04:51 +0530

    Updated image page

--- src/components/ImagePage.js
+++ src/components/ImagePage.js
@@ -1,7 +1,8 @@
+import './ImagePage.css';
 import { useState, useEffect } from 'react';
 import { getImage } from '../api/imageService';
 import { useParams } from 'react-router-dom';
-import { Button, Typography } from '@mui/material';
+import { Button } from '@mui/material';
 import TuneIcon from '@mui/icons-material/Tune';
 import CompareIcon from '@mui/icons-material/Compare';
 import DownloadIcon from '@mui/icons-material/Download';
@@ -43,69 +44,73 @@ export function ImagePage() {
     const originalImageUrl = encodeURI(getImageUrl('uploads',userId,image.image_id,image.filename));
     const compressedImageUrl =encodeURI(getImageUrl('resized',userId,image.image_id,image.filename));
 
+    const handleDownloadCompressed = () => {
+        window.open(compressedImageUrl);
+    }
+
     return (
         <div className="image-page-container">
             <div className="image-page-container-header">
                 <div className="image-page-container-header-left">
-                    <Typography variant="h4" style={{fontWeight: '500'}}>Image Comparison</Typography>
-                    <Typography variant="h6" style={{textAlign: 'left', width: '100%'}}>{image.filename}</Typography>
-                </div>
-                <div className="image-page-container-header-right">
-                    <Button variant={isSlider ? "contained" : "outlined"} color="primary" startIcon={<TuneIcon />} onClick={() => handlerSliderOrCompareClick(true)}>Slider</Button>
-                    <Button variant={!isSlider ? "contained" : "outlined"} color="primary" startIcon={<CompareIcon />} onClick={() => handlerSliderOrCompareClick(false)}>Side by Side</Button>
-                    <Button variant="contained" color="primary" startIcon={<DownloadIcon />}>Download Compressed</Button>
+                    <h2 className="title-font">Image Comparison</h2>
+                    <h3 className="title-font-small-no-accent">{image.filename}</h3>
                 </div>
             </div>
             <div className='image-compression-metadata-container'>
                 <div className='image-compression-metadata-card'>
-                <Typography variant="h6" style={{textAlign: 'left', width: '100%', fontWeight: '600'}}>Original Size</Typography>
-                <Typography variant="h6" style={{textAlign: 'left', width: '100%'}}>{originalSize} MB</Typography>
+                <h2 className="title-font-small-no-accent">Original Size</h2>
+                <h3 className="description-font">{originalSize} MB</h3>
                 </div>
                 <div className='image-compression-metadata-card'>
-                <Typography variant="h6" style={{textAlign: 'left', width: '100%', fontWeight: '600'}}>Compressed Size</Typography>
-                <Typography variant="h6" style={{textAlign: 'left', width: '100%'}}>{compressedSize} MB</Typography>
+                <h2 className="title-font-small-no-accent">Compressed Size</h2>
+                <h3 className="description-font">{compressedSize} MB</h3>
                 </div>
                 <div className='image-compression-metadata-card'>
-                    <Typography variant="h6" style={{textAlign: 'left', width: '100%', fontWeight: '600'}}>Compression Ratio</Typography>
-                    <Typography variant="h6" style={{textAlign: 'left', width: '100%'}}>{compressionRatio.toFixed(2)}%</Typography>
+                    <h2 className="title-font-small-no-accent">Compression Ratio</h2>
+                    <h3 className="description-font">{compressionRatio.toFixed(2)}%</h3>
                 </div>
             </div>
             <div className='image-compression-image-metadata-container'>
                 <div className='image-compression-image-metadata-title'>
-                    <Typography variant="h6" style={{textAlign: 'left', width: '100%', fontWeight: '600'}}>Image Details</Typography>
+                    <h2 className='title-font-small-no-accent'>Image Details</h2>
                 </div>
                 <div className='image-compression-image-metadata-row'>
                     <div className='image-compression-image-metadata-column'>
                         <div className='image-compression-image-metadata-item'>
-                            <Typography variant="h6" style={{textAlign: 'left', width: '100%', fontWeight: '200'}}>Dimensions</Typography>
-                            <Typography variant="h6" style={{textAlign: 'left', width: '100%', fontWeight: '500'}}>{image.width + "X" + image.height}</Typography>
+                            <h3 className='title-font-small-no-accent'>Dimensions</h3>
+                            <h3 className='description-font'>{image.width + "X" + image.height}</h3>
                         </div>
                         <div className='image-compression-image-metadata-item'>
-                            <Typography variant="h6" style={{textAlign: 'left', width: '100%', fontWeight: '200'}}>Uploaded At</Typography>
-                            <Typography variant="h6" style={{textAlign: 'left', width: '100%', fontWeight: '500'}}>{new Date(image.created_at).toLocaleString()}</Typography>
-                        </div>
-                    </div>
-                    <div className='image-compression-image-metadata-column'>    
-                        <div className='image-compression-image-metadata-item'>
-                            <Typography variant="h6" style={{textAlign: 'left', width: '100%', fontWeight: '200'}}>Format</Typography>
-                            <Typography variant="h6" style={{textAlign: 'left', width: '100%', fontWeight: '500'}}>{image.format?.toUpperCase()}</Typography>
-                        </div>
-                        <div className='image-compression-image-metadata-item'>
-                            <Typography variant="h6" style={{textAlign: 'left', width: '100%', fontWeight: '200'}}>Compressed At</Typography>
-                            <Typography variant="h6" style={{textAlign: 'left', width: '100%', fontWeight: '500'}}>{image.compressed_at?.Time? new Date(image.compressed_at?.Time).toLocaleString() : 'N/A'}</Typography>
+                            <h3 className='title-font-small-no-accent'>Uploaded At</h3>
+                            <h3 className='description-font'>{new Date(image.created_at).toLocaleString()}</h3>
                         </div>
                     </div>
                     <div className='image-compression-image-metadata-column'>    
                         <div className='image-compression-image-metadata-item'>
-                            <Typography variant="h6" style={{textAlign: 'left', width: '100%', fontWeight: '200'}}>Status</Typography>
-                            <Typography variant="h6" style={{textAlign: 'left', width: '100%', fontWeight: '500'}}>{toCamelCase("_" +image.job_status)}</Typography>
+                            <h3 className='title-font-small-no-accent'>Format</h3>
+                            <h3 className='description-font'>{image.format?.toUpperCase()}</h3>
                         </div>
                         <div className='image-compression-image-metadata-item'>
-                            <Typography variant="h6" style={{textAlign: 'left', width: '100%', fontWeight: '200'}}>Image ID</Typography>
-                            <Typography variant="h6" style={{textAlign: 'left', width: '100%', fontWeight: '500'}}>{image.image_id}</Typography>
+                            <h3 className='title-font-small-no-accent'>Compressed At</h3>
+                            <h3 className='description-font'>{image.compressed_at?.Time? new Date(image.compressed_at?.Time).toLocaleString() : 'N/A'}</h3>
+                        </div>
+                    </div>
+                    <div className='image-compression-image-metadata-column'>    
+                        <div className='image-compression-image-metadata-item'>
+                            <h3 className='title-font-small-no-accent'>Status</h3>
+                            <h3 className='description-font'>{toCamelCase("_" +image.job_status)}</h3>
+                        </div>
+                        <div className='image-compression-image-metadata-item'>
+                            <h3 className='title-font-small-no-accent'>Image ID</h3>
+                            <h3 className='description-font'>{image.image_id}</h3>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="image-page-container-buttons-container">
+                    <Button className={(isSlider ? 'image-page-container-buttons-active' : 'image-page-container-buttons')} startIcon={<TuneIcon />} onClick={() => handlerSliderOrCompareClick(true)}>Slider</Button>
+                    <Button className={(!isSlider ? 'image-page-container-buttons-active' : 'image-page-container-buttons')} startIcon={<CompareIcon />} onClick={() => handlerSliderOrCompareClick(false)}>Side by Side</Button>
+                    <Button className='image-page-container-buttons-active' startIcon={<DownloadIcon /> } onClick={() => handleDownloadCompressed()}>Download Compressed</Button>
             </div>
             <div className='image-comparison-container'>
                     {isSlider? <ReactCompareSlider 
@@ -120,7 +125,7 @@ export function ImagePage() {
                                 <div className='image-comparison-item-label'>Compressed</div>
                                 <ReactCompareSliderImage src={compressedImageUrl} alt="Image two" />
                             </div>
-                        } 
+                        }
                     />:
                     <div className='image-comparison-side-by-side-container'>
                         <div className='image-comparison-side-by-side-item-one'>
